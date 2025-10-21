e6d10111322ea80ea65f20f8148841974db99d63
sabilafirdias <sabilafirdias@gmail.com>
2025-10-21 14:27:20 +0700

    mendeklarasikan get dan listen

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const app = express();
 const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
